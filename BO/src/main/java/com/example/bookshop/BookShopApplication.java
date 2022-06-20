2aadc13a49b5430bf5b430776338e9eaf2d040a4
ka-chi <1104579593@qq.com>
2022-06-20 11:36:29 +0800

    nice

--- BO/src/main/java/com/example/bookshop/BookShopApplication.java
+++ BO/src/main/java/com/example/bookshop/BookShopApplication.java
@@ -12,6 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class BookShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookShopApplication.class, args);
-
+		int a =0;
     }
 }
